cb114756d396de4577ffc9af9cf44385a613f4e7
aisc521 <18210301859@163.com>
2019-05-22 10:28:58 +0800

    notift

--- zhcw-pay-service-web-api/src/main/java/com/zhcw/zfb/api/PayNotiftApi.java
+++ zhcw-pay-service-web-api/src/main/java/com/zhcw/zfb/api/PayNotiftApi.java
@@ -2,6 +2,9 @@ package com.zhcw.zfb.api;
 
 import com.google.gson.Gson;
 import org.json.JSONObject;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
@@ -13,7 +16,16 @@ import java.util.Map;
 /**
  * 接收异步
  */
+@RestController
+@RequestMapping("zhcw")
 public class PayNotiftApi {
+    /**
+     * 交易异步通知
+     * @param request
+     * @return
+     * @throws IOException
+     */
+    @PostMapping("/trade_notify")
     public Map notift(HttpServletRequest request) throws IOException {
         InputStream is = null;
         Map<String, Object> map = new HashMap<>();
@@ -36,7 +48,6 @@ public class PayNotiftApi {
             @SuppressWarnings("unchecked")
             Gson gson = new Gson();
             map = gson.fromJson(json, HashMap.class);
-            System.out.println(map);
         }  catch (Exception e) {
             e.printStackTrace();
         }  finally {
