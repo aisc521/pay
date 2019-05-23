199acacce29f9049c5c46068da8a139f881e5155
aisc521 <18210301859@163.com>
2019-05-23 10:35:07 +0800

    111

--- zhcw-pay-service-web-api/src/main/java/com/zhcw/zfb/api/PayNotiftApi.java
+++ zhcw-pay-service-web-api/src/main/java/com/zhcw/zfb/api/PayNotiftApi.java
@@ -1,21 +1,14 @@
 package com.zhcw.zfb.api;
 
-import com.google.gson.Gson;
 import com.zhcw.pay.utils.HttpUtils.RequestUtil;
 import com.zhcw.pay.utils.PayUtils.PayNotiftUtils;
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
 
 /**
