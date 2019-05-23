f1de0d0fc0be1ee4a11b0c02252ace5940490c90
aisc521 <18210301859@163.com>
2019-05-23 15:11:26 +0800

    111

--- zhcw-pay-service-web-api/src/main/java/com/zhcw/zfb/api/PayNotiftApi.java
+++ zhcw-pay-service-web-api/src/main/java/com/zhcw/zfb/api/PayNotiftApi.java
@@ -4,6 +4,7 @@ import com.zhcw.pay.utils.HttpUtils.RequestUtil;
 import com.zhcw.pay.utils.PayUtils.PayNotiftUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,27 +19,29 @@ import java.util.Map;
 @RequestMapping("zhcw")
 public class PayNotiftApi {
     private Logger logger = LoggerFactory.getLogger(PayNotiftApi.class);
+
     /**
      * 交易异步通知
+     *
      * @param request
      * @return
      * @throws IOException
      */
     @PostMapping("/trade_notify")
     //@GetMapping("/trade_notify")
-    public Map payNotift(HttpServletRequest request) throws IOException {
+    public String payNotift(HttpServletRequest request) throws IOException {
         logger.info("异步通知消息:==================start");
         Map map = PayNotiftUtils.notift(request);
-        logger.info("异步通知消息:==================end" + map.toString());
-        return map;
+        logger.info("异步通知消息:==================end==" + map.toString());
+        return "success";
     }
 
-    @PostMapping("/trade_notify1")
-    //@GetMapping("/trade_notify")
-    public Map payNotift1(HttpServletRequest request) throws IOException {
-        logger.info("异步通知消息:==================start");
+    @GetMapping("/call_back")
+    public String call_back(HttpServletRequest request) throws Exception {
+        logger.info("call_back:==================start");
+        //获取请求参数
         Map requestMap = RequestUtil.getParameters(request,true);
-        logger.info("异步通知消息:==================end" + requestMap.toString());
-        return requestMap;
+        logger.info("call_back:==================end" + requestMap.toString());
+        return "call_back_html=====hello!!!!!!";
     }
 }
